2e1cefdf1538c9ef3e68b0e653e01020461e481b
Quantis Project <info@quantisproject.com>
2015-10-18 19:49:15 +0200

    Rough cut.

---
+++ ObserveMany/ObserveMany/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObserveMany
+//
+//  Created by Akos Polster on 18/10/15.
+//  Copyright © 2015 Akos Polster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
